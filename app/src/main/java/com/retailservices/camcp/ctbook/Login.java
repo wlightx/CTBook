980bf9a71ab30046b28eefc1159be32e40d2c7a4
Camilo Contreras <cam.cp94@gmail.com>
2017-12-03 23:16:24 -0300

    He realizado el diseño del login, ademas he agregado componentes que permiten redondear el borde de botones y cuadros

--- app/src/main/java/com/retailservices/camcp/ctbook/Login.java
+++ app/src/main/java/com/retailservices/camcp/ctbook/Login.java
@@ -1,13 +1,27 @@
 package com.retailservices.camcp.ctbook;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class Login extends AppCompatActivity {
+
+    //region Vistas
+    private TextInputEditText etUserLogin, etPassLogin;
+    private Button btnIngresarLogin;
+    private RadioButton sesion;
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUserLogin = (TextInputEditText)findViewById(R.id.etUserLogin);
+        etPassLogin = (TextInputEditText)findViewById(R.id.etPassLogin);
+        btnIngresarLogin = (Button)findViewById(R.id.btnIngresarLogin);
+        sesion = (RadioButton) findViewById(R.id.rbSesionLogin);
     }
 }
